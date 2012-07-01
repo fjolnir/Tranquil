93225cdb1adbea1bbea2d306bf3d8235a05a56dc
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-07-01 21:35:35 +0900

    Cleanup

--- Source/Tranquil/gen_stubs.rb
+++ Source/Tranquil/gen_stubs.rb
@@ -17,7 +17,7 @@ extern \"C\" {
     source << "id TQDispatchBlock#{i}(struct TQBlockLiteral *block"; (1..i).each { |j| source << ", id a#{j}" }; source << ")
 {
     static void *underflowJmpTbl[] = { "
-    (i..maxArgs).each { |j| source << "&&underflow#{j-i}"; source << ", " unless j==maxArgs }; source << " };\n"
+    (i..maxArgs).each { |j| source << "&&underflow#{j-i}"; source << ", " unless j == maxArgs }; source << " };\n"
     source << "
     if(block->flags & TQ_BLOCK_IS_TRANQUIL_BLOCK) {
         if(block->descriptor->numArgs > #{maxArgs})
@@ -26,12 +26,12 @@ extern \"C\" {
             if(block->descriptor->numArgs <= #{i})
                 goto *underflowJmpTbl[0];
             else
-                goto *underflowJmpTbl[block->descriptor->numArgs"; source << " - #{i}" unless i==0; source <<"];
+                goto *underflowJmpTbl[block->descriptor->numArgs"; source << " - #{i}" unless i == 0; source <<"];
         } else {
             if(block->descriptor->numArgs == #{i})
                 return block->invoke(block"; (1..i).each { |j| source << ", a#{j}"; }; source << ");
             else if(block->descriptor->numArgs > #{i})
-                goto *underflowJmpTbl[block->descriptor->numArgs"; source << "- #{i+1}" unless i==0; source <<"];
+                goto *underflowJmpTbl[block->descriptor->numArgs"; source << "- #{i+1}" unless i == 0; source <<"];
             else if(block->descriptor->numArgs < #{i})
                 TQAssert(NO, @\"Too many arguments to %@! #{i} for %d\", block, block->descriptor->numArgs);
         }
@@ -39,7 +39,7 @@ extern \"C\" {
         return block->invoke(block"; (1..i).each { |j| source << ", a#{j}"; }; source << ");
 
 "
-    (i..maxArgs).each do |j| 
+    (i..maxArgs).each do |j|
     source << "    underflow#{j-i}:\n"
     source << "        return block->invoke(block";
         (1..i).each { |k| source << ", a#{k}" };
