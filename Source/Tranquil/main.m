f31a6bc9f9a0c0cd23d9f3a7b92704e0e8444c97
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-08-17 01:04:13 +0900

    Cleanup

--- Source/Tranquil/main.m
+++ Source/Tranquil/main.m
@@ -11,34 +11,34 @@ void printHelpAndExit(int status)
 int main(int argc, char **argv)
 {
     @autoreleasepool {
-    BOOL showDebugOutput = NO;
-    char *inputPath = NULL;
+        BOOL showDebugOutput = NO;
+        char *inputPath = NULL;
 
-    char *arg;
-    for(int i = 1; i < argc; ++i) {
-        arg = argv[i];
-        if(arg[0] == '-') {
-            if(strcmp(arg, "-d") == 0) showDebugOutput = YES;
-            else if(strcmp(arg, "-h") == 0) printHelpAndExit(0);
-            else {
-                fprintf(stderr, "Unknown argument %s\n", arg);
-                printHelpAndExit(1);
-            }
-        } else
-            inputPath = arg;
-    }
+        char *arg;
+        for(int i = 1; i < argc; ++i) {
+            arg = argv[i];
+            if(arg[0] == '-') {
+                if(strcmp(arg, "-d") == 0) showDebugOutput = YES;
+                else if(strcmp(arg, "-h") == 0) printHelpAndExit(0);
+                else {
+                    fprintf(stderr, "Unknown argument %s\n", arg);
+                    printHelpAndExit(1);
+                }
+            } else
+                inputPath = arg;
+        }
 
-    TQProgram *program = [TQProgram programWithName:@"Root"];
-    program.shouldShowDebugInfo = showDebugOutput;
-    NSString *script;
-    if(inputPath)
-        script = [NSString stringWithContentsOfFile:[NSString stringWithUTF8String:inputPath] encoding:NSUTF8StringEncoding error:nil];
-    else {
-        NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
-        NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
-        script = [[[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding] autorelease];
-    }
-    [program executeScript:script error:nil];
+        TQProgram *program = [TQProgram programWithName:@"Root"];
+        program.shouldShowDebugInfo = showDebugOutput;
+        NSString *script;
+        if(inputPath)
+            [program executeScriptAtPath:[NSString stringWithUTF8String:inputPath] error:nil];
+        else {
+            NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
+            NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
+            script = [[[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding] autorelease];
+            [program executeScript:script error:nil];
+        }
     }
     return 0;
 }
