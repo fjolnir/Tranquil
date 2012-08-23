8f793f054df5f1ba7f94f1bcfe81cfe1827449a1
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-08-23 17:08:33 +0900

    Added a preprocessor that inserts super dealloc

---
+++ Source/Tranquil/CodeGen/Processors/TQDeallocInsertProcessor.m
@@ -0,0 +1,31 @@
+#import "TQDeallocInsertProcessor.h"
+#import "../TQNodeMethod.h"
+#import "../TQNodeMessage.h"
+#import "../TQNodeVariable.h"
+#import "../TQNodeArgument.h"
+
+@implementation TQDeallocInsertProcessor
++ (void)load
+{
+    if(self != [TQDeallocInsertProcessor class])
+        return;
+    [TQProcessor registerProcessor:self];
+}
+
++ (TQNode *)processNode:(TQNode *)aNode withTrace:(NSArray *)aTrace
+{
+    if(![aNode isKindOfClass:[TQNodeMethod class]])
+        return aNode;
+    TQNodeMethod *method = (TQNodeMethod *)aNode;
+
+    if(method.type == kTQInstanceMethod && [[method selector] isEqualToString:@"dealloc"]) {
+        
+        TQNodeMessage *superDealloc = [TQNodeMessage nodeWithReceiver:[TQNodeSuper node]];
+        [superDealloc.arguments addObject:[TQNodeArgument nodeWithPassedNode:nil selectorPart:@"dealloc"]];
+        if(![method referencesNode:superDealloc])
+            [method.statements addObject:superDealloc];
+    }
+
+    return aNode;
+}
+@end
