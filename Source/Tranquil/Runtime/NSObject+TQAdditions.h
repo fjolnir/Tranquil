7c9b03c71864be75bb118e71a01198ffebad67e4
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-08-27 10:44:06 +0900

    Implemented an include: method on NSObject that copies methods from a specified class.

--- Source/Tranquil/Runtime/NSObject+TQAdditions.h
+++ Source/Tranquil/Runtime/NSObject+TQAdditions.h
@@ -1,6 +1,9 @@
 #import <Foundation/Foundation.h>
+@class TQNumber;
 
 @interface NSObject (Tranquil)
 - (NSMutableString *)toString;
 - (id)print;
++ (id)include:(Class)aClass recursive:(TQNumber *)aRecursive;
++ (id)include:(Class)aClass;
 @end
--- Source/Tranquil/Runtime/NSObject+TQAdditions.m
+++ Source/Tranquil/Runtime/NSObject+TQAdditions.m
@@ -1,4 +1,7 @@
 #import "NSObject+TQAdditions.h"
+#import "TQNumber.h"
+#import "TQRuntime.h"
+#import <objc/runtime.h>
 
 @implementation NSObject (Tranquil)
 - (NSMutableString *)toString
@@ -10,5 +13,32 @@
 {
     printf("%s\n", [[self description] UTF8String]);
     return self;
+}
+
+#define CopyMethods(kls, dst) do { \
+    unsigned methodCount = 0; \
+    Method *methods = class_copyMethodList(kls, &methodCount); \
+    for(int i = 0; i < methodCount; ++i) { \
+        class_addMethod(dst, method_getName(methods[i]), method_getImplementation(methods[i]), method_getTypeEncoding(methods[i])); \
+    } \
+    free(methods); \
+} while(0);
+
++ (id)include:(Class)aClass recursive:(TQNumber *)aRecursive
+{
+    if([aClass isKindOfClass:[NSString class]])
+        aClass = NSClassFromString((NSString *)aClass);
+    TQAssert(aClass, @"Tried to include nil class");
+
+    do {
+        CopyMethods(object_getClass(aClass), object_getClass(self));
+        CopyMethods(aClass, self);
+    } while([aRecursive boolValue] && (aClass = class_getSuperclass(aClass)));
+    return TQValid;
+}
+#undef CopyMethods
++ (id)include:(Class)aClass
+{
+    return [self include:aClass recursive:nil];
 }
 @end
