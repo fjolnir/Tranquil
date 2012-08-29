0a56ddc492e98e4480816950e699163186ba81ef
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-08-30 03:17:36 +0900

    Used faster ruby comparison

--- Docs/Reference/Comparisons/fib.rb
+++ Docs/Reference/Comparisons/fib.rb
@@ -1,17 +1,17 @@
-#def fib(n)
-#    if n < 2 then
-#        return n
-#    else
-#        return fib(n-1) + fib(n-2)
-#    end
-#end
-fib = lambda { |n|
+def fib(n)
     if n < 2 then
         return n
     else
-        return fib[n-1] + fib[n-2]
+        return fib(n-1) + fib(n-2)
     end
-}
-p fib
-p fib[35]
+end
+#fib = lambda { |n|
+    #if n < 2 then
+        #return n
+    #else
+        #return fib[n-1] + fib[n-2]
+    #end
+#}
+#p fib
+p fib(35)
 
