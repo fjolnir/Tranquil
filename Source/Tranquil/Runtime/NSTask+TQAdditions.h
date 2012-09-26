9444a89e0359fe4a1875f6c972358c9f85340b57
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-09-26 18:02:59 +0900

    Added a nicer way to use NSTask

---
+++ Source/Tranquil/Runtime/NSTask+TQAdditions.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+@interface NSTask (Tranquil)
++ (NSString *)execute:(NSString *)aPath with:(NSArray *)aArguments;
+@end
---
+++ Source/Tranquil/Runtime/NSTask+TQAdditions.m
@@ -0,0 +1,24 @@
+#import "NSTask+TQAdditions.h"
+
+@implementation NSTask (Tranquil)
++ (NSString *)execute:(NSString *)aPath with:(NSArray *)aArguments;
+{
+    NSPipe *pipe = [NSPipe pipe];
+    NSTask *task = [NSTask new];
+    [task setLaunchPath:aPath];
+    if([aArguments isKindOfClass:[NSArray class]] ||
+       [aArguments isKindOfClass:[NSPointerArray class]]) {
+        [task setArguments:aArguments];
+    } else if([aArguments isKindOfClass:[NSString class]])
+        [task setArguments:[NSArray arrayWithObject:aArguments]];
+    [task setStandardOutput:pipe];
+    [task setStandardError:[NSFileHandle fileHandleWithStandardError]];
+    [task setStandardInput:[NSFileHandle fileHandleWithStandardInput]];
+    [task launch];
+    [task waitUntilExit];
+    [task release];
+
+    NSData *data = [[pipe fileHandleForReading] availableData];
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+}
+@end
