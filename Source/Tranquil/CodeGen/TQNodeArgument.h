cb6154082daa24c90614c479c6d5abac69bcc5f6
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-11-25 12:31:01 +0900

    Cleanup

--- Source/Tranquil/CodeGen/TQNodeArgument.h
+++ Source/Tranquil/CodeGen/TQNodeArgument.h
@@ -3,7 +3,7 @@
 // An argument to block (name: arg)
 @interface TQNodeArgument : TQNode
 @property(readwrite, retain) NSString *selectorPart;  // The argument identifier, that is, the portion before ':'
-@property(readwrite, retain) TQNode *passedNode;    // The node after ':'
+@property(readwrite, retain) TQNode *passedNode; // The node after ':'
 
 + (TQNodeArgument *)nodeWithPassedNode:(TQNode *)aNode selectorPart:(NSString *)aIdentifier;
 - (id)initWithPassedNode:(TQNode *)aNode selectorPart:(NSString *)aIdentifier;
