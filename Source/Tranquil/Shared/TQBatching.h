dc3f3b224d28261e6b333df0b95ad3a402374208
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-11-28 11:23:38 +0900

    Made batching thread safe (Would be nice to do this without the spinlock)

--- Source/Tranquil/Shared/TQBatching.h
+++ Source/Tranquil/Shared/TQBatching.h
@@ -2,6 +2,7 @@
 #import <Tranquil/Shared/TQDebug.h>
 #import <objc/runtime.h>
 #import <objc/message.h>
+#import <libkern/OSAtomic.h>
 
 // the size needed for the batch, with proper alignment for objects
 #define ALIGNMENT           8
@@ -23,6 +24,7 @@ typedef struct
     uintptr_t low, high;
     TQBatch   *currentBatch;
     TQBatch   **batches;
+    OSSpinLock spinLock;
 } TQBatchPool;
 
 static inline TQBatch *TQNewObjectBatch(TQBatchPool *pool, long batchInstanceSize)
@@ -60,8 +62,7 @@ static inline void TQRecycleObjectBatch(TQBatchPool *pool, TQBatch *batch)
         batch->_allocated = 0;
         pool->batches[pool->high] = batch;
         pool->high = next;
-        if(pool->currentBatch == batch)
-            pool->currentBatch = pool->batches[next];
+        __sync_val_compare_and_swap(&pool->currentBatch, batch, pool->batches[next]);
     }
 }
 
@@ -76,7 +77,6 @@ static inline BOOL TQBatchIsExhausted(TQBatch *p)
     return p->_allocated == OBJECTS_PER_BUNCH;
 }
 
-
 #define TQ_BATCH_IVARS                                                                         \
     /* The batch the object is in */                                                           \
     TQBatch *_batch;                                                                           \
@@ -88,24 +88,21 @@ static TQBatchPool _BatchPool;                                                  
                                                                                                \
 static inline Klass *TQBatchAlloc##Klass(Class self)                                           \
 {                                                                                              \
-    if(!_BatchPool.batches) {                                                                  \
+    size_t instanceSize = class_getInstanceSize(self);                                         \
+    OSSpinLockLock(&_BatchPool.spinLock);                                                      \
+    if(__builtin_expect(!_BatchPool.batches, 0)) {                                             \
         _BatchPool.poolSize = PoolSize;                                                        \
         _BatchPool.batches  = (TQBatch **)malloc(sizeof(void*) * _BatchPool.poolSize);         \
-    }                                                                                          \
-                                                                                               \
-    /* First time? => Allocate & initialize a new batch */                                     \
-    size_t instanceSize = class_getInstanceSize(self);                                         \
-    if(!_BatchPool.currentBatch)                                                               \
         _BatchPool.currentBatch = TQNewObjectBatch(&_BatchPool, instanceSize);                 \
+    }                                                                                          \
                                                                                                \
     Klass *obj = nil;                                                                          \
     TQBatch *batch = _BatchPool.currentBatch;                                                  \
-    if(TQSizeFitsObjectBatch(_BatchPool.currentBatch, instanceSize))                           \
+    if(__builtin_expect(TQSizeFitsObjectBatch(_BatchPool.currentBatch, instanceSize), 1))      \
     {                                                                                          \
         /* Grab an object from the current batch */                                            \
         /* and place isa pointer there */                                                      \
         NSUInteger offset;                                                                     \
-                                                                                               \
         offset      = BatchSize + batch->_instance_size * batch->_allocated;                   \
         obj         = (id)((char *)batch + offset);                                            \
         obj->_batch = batch;                                                                   \
@@ -113,13 +110,15 @@ static inline Klass *TQBatchAlloc##Klass(Class self)                            
                                                                                                \
         batch->_allocated++;                                                                   \
         *(Class *)obj = self;                                                                  \
+    } else {                                                                                   \
+        TQAssert(NO, @"Unable to get %@ from batch", self);                                    \
     }                                                                                          \
-    assert(obj != nil);                                                                        \
                                                                                                \
     /* Batch full? => Make a new one for next time */                                          \
     if(TQBatchIsExhausted(batch) && _BatchPool.currentBatch == batch)                          \
         _BatchPool.currentBatch = TQNewObjectBatch(&_BatchPool, instanceSize);                 \
                                                                                                \
+    OSSpinLockUnlock(&_BatchPool.spinLock);                                                    \
     return obj;                                                                                \
 }                                                                                              \
                                                                                                \
@@ -133,13 +132,15 @@ static inline Klass *TQBatchAlloc##Klass(Class self)                            
 }                                                                                              \
 - (oneway void)release                                                                         \
 {                                                                                              \
-    if(__sync_sub_and_fetch(&_retainCountMinusOne, 1) < 0)                                     \
+    if(__builtin_expect(__sync_sub_and_fetch(&_retainCountMinusOne, 1) < 0, 0))                \
         [self dealloc];                                                                        \
 }
 
 #define TQ_BATCH_DEALLOC                                                                       \
     /* Recycle the entire batch if all the objects in it are unreferenced */                   \
-    if(__sync_add_and_fetch(&_batch->_freed, 1) == OBJECTS_PER_BUNCH)                          \
+    if(__sync_add_and_fetch(&_batch->_freed, 1) == OBJECTS_PER_BUNCH) {                        \
+        OSSpinLockLock(&_BatchPool.spinLock);                                                  \
         TQRecycleObjectBatch(&_BatchPool, _batch);                                             \
+        OSSpinLockUnlock(&_BatchPool.spinLock);                                                \
+    }                                                                                          \
     else if(NO) [super dealloc]; /* Silence compiler warning about not calling super dealloc */
-
