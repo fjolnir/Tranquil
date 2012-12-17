db5fe90d00d73ac9e2d8f6c9b2982d71a69e0418
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-12-17 15:22:28 +0900

    Cleanup

--- Docs/Reference/Comparisons/fib.rb
+++ Docs/Reference/Comparisons/fib.rb
@@ -6,11 +6,11 @@ def fib(n)
     end
 end
 #fib = lambda { |n|
-    #if n < 2 then
-        #return n
-    #else
-        #return fib[n-1] + fib[n-2]
-    #end
+#    if n < 2 then
+#        return n
+#    else
+#        return fib[n-1] + fib[n-2]
+#    end
 #}
 #p fib
 p fib(35)
