28e02aab8497df6873c1be8e01c50d194262f642
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2013-11-16 02:52:52 +0900

    Adjusted fibonacci test

--- Docs/Reference/Comparisons/fib.rb
+++ Docs/Reference/Comparisons/fib.rb
@@ -1,10 +1,17 @@
-def fib(n)
+def fib(n, a=0, b=1)
+#    if n == 0 then
+#        return a
+#    else
+#        return fib(n-1, b, a+b)
+#    end
     if n < 2 then
         return n
     else
         return fib(n-1) + fib(n-2)
     end
 end
+p fib(35)
+
 #fib = lambda { |n|
 #    if n < 2 then
 #        return n
@@ -12,6 +19,5 @@ end
 #        return fib[n-1] + fib[n-2]
 #    end
 #}
-#p fib
-p fib(35)
+#p fib[35]
 
